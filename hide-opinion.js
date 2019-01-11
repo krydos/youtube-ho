ccc360c52e25d4e10bbcbfbfb1e73b465b4aff22
Ruslan Bekenev <furyinbox@gmail.com>
2019-01-11 20:26:06 +0300

    youtube enchancer support

--- hide-opinion.js
+++ hide-opinion.js
@@ -1,9 +1,23 @@
-const elementsToHide = {
-  likes: document.querySelector('ytd-toggle-button-renderer.ytd-menu-renderer:nth-child(1) > a:nth-child(1) > yt-formatted-string:nth-child(2)'),
-  dislikes: document.querySelector('ytd-toggle-button-renderer.style-scope:nth-child(2) > a:nth-child(1) > yt-formatted-string:nth-child(2)'),
-  bar: document.querySelector('#sentiment'),
-};
+const likeSelectors = [
+  'ytd-toggle-button-renderer.ytd-menu-renderer:nth-child(1) > a:nth-child(1) > yt-formatted-string:nth-child(2)',
+  '.like-button-renderer-like-button-unclicked > span:nth-child(1)',
+];
+const dislikeSelectors = [
+  'ytd-toggle-button-renderer.style-scope:nth-child(2) > a:nth-child(1) > yt-formatted-string:nth-child(2)',
+  '.like-button-renderer-dislike-button-unclicked > span:nth-child(1)'
+];
+const barSelectors = [
+  '#sentiment',
+  '.video-extras-sparkbars',
+];
 
-Object.keys(elementsToHide).forEach(function(el) {
-  elementsToHide[el].style.visibility = 'hidden';
+const elementsToHide = [].concat(likeSelectors, dislikeSelectors, barSelectors).map(function(selector) {
+  return document.querySelector(selector);
+});
+
+
+elementsToHide.forEach(function(el) {
+  if (!el) return;
+
+  el.style.visibility = 'hidden';
 });
