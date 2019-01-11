c593aa3d62bb96f6362c6cb5e2ee3f61d8d4a250
Ruslan Bekenev <furyinbox@gmail.com>
2019-01-11 20:35:43 +0300

    add one more element to hide when youtube enchancer is using

--- hide-opinion.js
+++ hide-opinion.js
@@ -1,10 +1,12 @@
 const likeSelectors = [
   'ytd-toggle-button-renderer.ytd-menu-renderer:nth-child(1) > a:nth-child(1) > yt-formatted-string:nth-child(2)',
   '.like-button-renderer-like-button-unclicked > span:nth-child(1)',
+  '.like-button-renderer-like-button-clicked > span:nth-child(1)',
 ];
 const dislikeSelectors = [
   'ytd-toggle-button-renderer.style-scope:nth-child(2) > a:nth-child(1) > yt-formatted-string:nth-child(2)',
-  '.like-button-renderer-dislike-button-unclicked > span:nth-child(1)'
+  '.like-button-renderer-dislike-button-unclicked > span:nth-child(1)',
+  '.like-button-renderer-dislike-button-clicked > span:nth-child(1)',
 ];
 const barSelectors = [
   '#sentiment',
