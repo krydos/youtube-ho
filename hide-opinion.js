011aeb9881b3c112b16c2f71ab8ed631c5a95795
Ruslan Bekenev <furyinbox@gmail.com>
2022-12-08 21:39:28 +1100

    Remove likes amount in new yt layout

--- hide-opinion.js
+++ hide-opinion.js
@@ -1,24 +1,34 @@
-const likeSelectors = [
+// set visibility:hidden so the area of an element is still in place
+const toHide = [
+  // likes
   'ytd-toggle-button-renderer.ytd-menu-renderer:nth-child(1) > a:nth-child(1) > yt-formatted-string:nth-child(2)',
   '.like-button-renderer-like-button-unclicked > span:nth-child(1)',
   '.like-button-renderer-like-button-clicked > span:nth-child(1)',
-];
-const dislikeSelectors = [
+  // dislikes
   'ytd-toggle-button-renderer.style-scope:nth-child(2) > a:nth-child(1) > yt-formatted-string:nth-child(2)',
   '.like-button-renderer-dislike-button-unclicked > span:nth-child(1)',
   '.like-button-renderer-dislike-button-clicked > span:nth-child(1)',
-];
-const barSelectors = [
+  // ratio bar
   '#sentiment',
   '.video-extras-sparkbars',
+];
+
+// set display:none
+const toRemove = [
+    '#segmented-like-button > ytd-toggle-button-renderer > yt-button-shape > button > div.cbox.yt-spec-button-shape-next--button-text-content'
 ];
 
 let styleString =  '';
 
 // build a css string
-const elementsToHide = [].concat(likeSelectors, dislikeSelectors, barSelectors).map(function(selector) {
-  styleString += selector + '{visibility:hidden;}';
-});
+const elementsToHide = [
+  ...toHide.map(function(selector) {
+      styleString += selector + '{visibility:hidden;}';
+  }),
+  ...toRemove.map(function(selector) {
+      styleString += selector + '{display:none;}';
+  })];
+
 
 // insert new css
 const head = document.head;
